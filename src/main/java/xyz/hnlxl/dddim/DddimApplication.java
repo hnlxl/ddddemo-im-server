7f6a0d3b42795e1b34a106e5393344a90d050cef
Oliver Liu <11850956+hnlxl@users.noreply.github.com>
2022-01-27 15:30:17 +0800

    fix style of generated code

--- src/main/java/xyz/hnlxl/dddim/DddimApplication.java
+++ src/main/java/xyz/hnlxl/dddim/DddimApplication.java
@@ -3,11 +3,12 @@ package xyz.hnlxl.dddim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** The application launcher */
 @SpringBootApplication
 public class DddimApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DddimApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DddimApplication.class, args);
+  }
 
 }
